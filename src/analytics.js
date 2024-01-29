3f09bae240d05c863c024afda8f8860ee2a13243
Mirosław Stanek <froger.mcs@gmail.com>
2024-01-29 21:14:35 +0100

    moved tokens to env variables

--- src/analytics.js
+++ src/analytics.js
@@ -6,8 +6,8 @@ const analytics = Analytics({
     app: 'example-app',
     version: 1.0,
     plugins: [
-        mixpanelPlugin({ token: 'ec3e18e109fc46c07a4f0db7aa457d9b' }),
-        amplitudePlugin({ apiKey: '915b4db61dfc9fd78f2f09d701f8db5e' })
+        mixpanelPlugin({ token: process.env.MIXPANEL_TOKEN }),
+        amplitudePlugin({ apiKey: process.env.AMPLITUDE_API_KEY })
     ]
 })
 
