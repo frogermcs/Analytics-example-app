bab2f447b51e0b7d53a46824adb8a754b956ac4d
Mirosław Stanek <froger.mcs@gmail.com>
2024-01-31 14:10:22 +0000

    bring back hardcoded tokens for debugging time

--- src/analytics.js
+++ src/analytics.js
@@ -6,10 +6,12 @@ const analytics = Analytics({
     app: 'example-app',
     version: 1.0,
     plugins: [
-        mixpanelPlugin({ token: process.env.MIXPANEL_TOKEN }),
-        amplitudePlugin({ apiKey: process.env.AMPLITUDE_API_KEY })
+        mixpanelPlugin({ token: 'ec3e18e109fc46c07a4f0db7aa457d9b' }),
+        amplitudePlugin({ apiKey: '915b4db61dfc9fd78f2f09d701f8db5e' })
     ]
 })
+
+console.log('env variable:', process.env.MIXPANEL_TOKEN)
 
 window.Analytics = analytics;
 export default analytics
